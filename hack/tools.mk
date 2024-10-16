9ee148bea0dd007fe350b9701c61fed4986db432
The Wok <32639489+Duciwuci@users.noreply.github.com>
2024-10-16 17:02:23 +0200

    bump Gardener to `v1.99` (#97)
    * bump Gardener to v1.99
    
    * fix make target
    
    * Run make generate
    
    ---------
    
    Co-authored-by: Marcel Boehm <marcel.boehm@inovex.de>

--- hack/tools.mk
+++ hack/tools.mk
@@ -5,21 +5,15 @@
 # as needed. If the required tool (version) is not built/installed yet, make will make sure to build/install it.
 # The *_VERSION variables in this file contain the "default" values, but can be overwritten in the top level make file.
 
-TOOLS_BIN_DIR              := $(TOOLS_DIR)/bin
-KO                         := $(TOOLS_BIN_DIR)/ko
 DEEPCOPY_GEN               := $(TOOLS_BIN_DIR)/deepcopy-gen
 DEFAULTER_GEN              := $(TOOLS_BIN_DIR)/defaulter-gen
 
 # default tool versions
-KO_VERSION ?= v0.15.0
 CODE_GENERATOR_VERSION ?= v0.29.5
 
 #########################################
 # Tools                                 #
 #########################################
-
-$(KO): $(call tool_version_file,$(KO),$(KO_VERSION))
-	GOBIN=$(abspath $(TOOLS_BIN_DIR)) go install github.com/google/ko@$(KO_VERSION)
 
 $(DEEPCOPY_GEN):$(call tool_version_file,$(DEEPCOPY_GEN),$(CODE_GENERATOR_VERSION))
 	GOBIN=$(abspath $(TOOLS_BIN_DIR)) go install k8s.io/code-generator/cmd/deepcopy-gen@$(CODE_GENERATOR_VERSION)
