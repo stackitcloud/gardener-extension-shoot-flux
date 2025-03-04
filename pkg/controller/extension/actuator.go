d859525e677073bd50962735c1e2bd221e912908
Niclas Schad <niclas.schad@stackit.cloud>
2025-03-04 22:25:36 +0100

    run ReconcileShootInfoConfigMap after flux has been fully boostrapped
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/controller/extension/actuator.go
+++ pkg/controller/extension/actuator.go
@@ -76,15 +76,15 @@ func (a *actuator) Reconcile(ctx context.Context, log logr.Logger, ext *extensio
 		return fmt.Errorf("error creating shoot client: %w", err)
 	}
 
-	if err := ReconcileShootInfoConfigMap(ctx, log, shootClient, config, cluster); err != nil {
-		return fmt.Errorf("error reconciling ConfigMap %q: %w", shootInfoConfigMapName, err)
-	}
-
 	if IsFluxBootstrapped(ext) {
 		log.V(1).Info("Flux installation has been bootstrapped already, will only reconcile secrets")
 
 		if err := ReconcileSecrets(ctx, log, a.client, shootClient, ext.Namespace, config, cluster.Shoot.Spec.Resources); err != nil {
 			return fmt.Errorf("error reconciling secrets: %w", err)
+		}
+
+		if err := ReconcileShootInfoConfigMap(ctx, log, shootClient, config, cluster); err != nil {
+			return fmt.Errorf("error reconciling ConfigMap %q: %w", shootInfoConfigMapName, err)
 		}
 
 		return nil
