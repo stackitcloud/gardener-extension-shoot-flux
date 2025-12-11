7f2e5874c2f05325e8f3051bf1ae82f111226706
eldr0n <38736006+eldr0n@users.noreply.github.com>
2025-12-11 17:28:16 +0100

    Add strconv import

--- pkg/controller/extension/secrets.go
+++ pkg/controller/extension/secrets.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"maps"
+	"strconv"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
